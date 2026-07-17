bbf42d3e9df77d05c01c1d9a571370c988a1b86c
Michelsantosmss <michelsantos.silva@yahoo.com.br>
2026-07-17 18:31:01 -0300

    fix: centraliza logo no app

--- src/components/EletroGridMark.tsx
+++ src/components/EletroGridMark.tsx
@@ -1,11 +1,9 @@
-import markReference from '../assets/eletrogrid-reference.jpg';
-
 type EletroGridMarkProps = { className?: string };
 
 export function EletroGridMark({ className = 'brand-mark' }: EletroGridMarkProps) {
   return (
     <span className={className} role="img" aria-label="EletroGrid">
-      <img src={markReference} alt="" />
+      <img src="/pwa-512x512.png" alt="" />
     </span>
   );
 }
