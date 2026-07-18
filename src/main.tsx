c2d08162241c5ecd625688da6f9370edb7075bd6
Michelsantosmss <michelsantos.silva@yahoo.com.br>
2026-07-18 03:42:05 -0300

    feat: activate quotation form redesign

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { installQuoteFormEnhancer } from './services/quoteFormEnhancer';
 import './styles.css';
+import './quote-redesign.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+installQuoteFormEnhancer();
