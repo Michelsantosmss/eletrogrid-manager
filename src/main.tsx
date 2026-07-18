1f675980e294200f826fe785fdb8bd48d8b2a345
Michelsantosmss <michelsantos.silva@yahoo.com.br>
2026-07-18 15:28:56 -0300

    feat: activate equipment intake module

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { installQuoteFormEnhancer } from './services/quoteFormEnhancer';
+import { installIntakeEnhancer } from './services/intakeEnhancer';
 import './styles.css';
 import './quote-redesign.css';
 import './mobile.css';
+import './intake.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -13,3 +15,4 @@ createRoot(document.getElementById('root')!).render(
 );
 
 installQuoteFormEnhancer();
+installIntakeEnhancer();
